03fcbfebfc8c9b8e4c4f4865dcf604d49074852d
Tam Long <aqua.tamlong@gmail.com>
2024-04-02 15:22:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pixelcraft
+
+The monospaced font for developers who like Minecraft a bit _too_ much.
+
+*Notice: This project is not affiliated with Minecraft or Mojang in any way and is exclusively a fan project. This font emulates the typeface of the font used in the Minecraft UI, but it does not include any assets or font files from the original game.*
+
+## How to install
+
+### Windows
+
+Download the most recent `Pixelcraft.ttf` file from the [Releases](https://github.com/aquapaka/Pixelcraft/releases) page. Right click on the downloaded font and select **Install**. You might need administrative access to install fonts, depending on your machine.
+
+## How to use
+
+After following the installation instructions up above, simply select the "Pixelcraft" font (note the space) in any application that supports custom fonts. You might need to restart the application or your computer for the font to appear.
---
+++ src/monocraft.py
@@ -0,0 +1,147 @@
+# Pixelcraft, a monospaced font for developers who like Minecraft a bit too much.
+# Copyright (C) 2022-2023 Idrees Hassan
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import fontforge
+import json
+import math
+from generate_diacritics import generateDiacritics
+from generate_examples import generateExamples
+from polygonizer import PixelImage, generatePolygons
+from generate_continuous_ligatures import generate_continuous_ligatures
+
+PIXEL_SIZE = 120
+
+characters = json.load(open("./characters.json", encoding="utf-8"))
+diacritics = json.load(open("./diacritics.json", encoding="utf-8"))
+ligatures = json.load(open("./ligatures.json", encoding="utf-8"))
+ligatures += generate_continuous_ligatures("./continuous_ligatures.json")
+
+characters = generateDiacritics(characters, diacritics)
+charactersByCodepoint = {}
+
+def generateFont():
+	pixelcraft = fontforge.font()
+	pixelcraft.fontname = "Pixelcraft"
+	pixelcraft.familyname = "Pixelcraft"
+	pixelcraft.fullname = "Pixelcraft"
+	pixelcraft.copyright = "Aquapaka, https://github.com/aquapaka/Pixelcraft"
+	pixelcraft.encoding = "UnicodeFull"
+	pixelcraft.version = "1.0"
+	pixelcraft.weight = "Regular"
+	pixelcraft.ascent = PIXEL_SIZE * 8
+	pixelcraft.descent = PIXEL_SIZE
+	pixelcraft.em = PIXEL_SIZE * 9
+	pixelcraft.upos = -PIXEL_SIZE # Underline position
+	pixelcraft.addLookup("ligatures", "gsub_ligature", (), (("liga",(("dflt",("dflt")),("latn",("dflt")))),))
+	pixelcraft.addLookupSubtable("ligatures", "ligatures-subtable")
+
+	for character in characters:
+		charactersByCodepoint[character["codepoint"]] = character
+		pixelcraft.createChar(character["codepoint"], character["name"])
+		pen = pixelcraft[character["name"]].glyphPen()
+		top = 0
+		drawn = character
+
+		image, kw = generateImage(character)
+		drawImage(image, pen, **kw)
+		pixelcraft[character["name"]].width = PIXEL_SIZE * 6
+	print(f"Generated {len(characters)} characters")
+
+	outputDir = "../dist/"
+	if not os.path.exists(outputDir):
+		os.makedirs(outputDir)
+
+	pixelcraft.generate(outputDir + "Pixelcraft-no-ligatures.ttf")
+	for ligature in ligatures:
+		lig = pixelcraft.createChar(-1, ligature["name"])
+		pen = pixelcraft[ligature["name"]].glyphPen()
+		image, kw = generateImage(ligature)
+		drawImage(image, pen, **kw)
+		pixelcraft[ligature["name"]].width = PIXEL_SIZE * len(ligature["sequence"]) * 6
+		lig.addPosSub("ligatures-subtable", tuple(map(lambda codepoint: charactersByCodepoint[codepoint]["name"], ligature["sequence"])))
+	print(f"Generated {len(ligatures)} ligatures")
+
+	pixelcraft.generate(outputDir + "Pixelcraft.ttf")
+	pixelcraft.generate(outputDir + "Pixelcraft.otf")
+
+def generateImage(character):
+	image = PixelImage()
+	kw = {}
+	if "pixels" in character:
+		arr = character["pixels"]
+		leftMargin = character["leftMargin"] if "leftMargin" in character else 0
+		x = math.floor(leftMargin)
+		kw['dx'] = leftMargin - x
+		descent = -character["descent"] if "descent" in character else 0
+		y = math.floor(descent)
+		kw['dy'] = descent - y
+		image = image | imageFromArray(arr, x, y)
+	if "reference" in character:
+		other = generateImage(charactersByCodepoint[character["reference"]])
+		kw.update(other[1])
+		image = image | other[0]
+	if "diacritic" in character:
+		diacritic = diacritics[character["diacritic"]]
+		arr = diacritic["pixels"]
+		x = image.x
+		y = findHighestY(image) + 1
+		# Side horn
+		if character["diacritic"] == "horn":
+			x = findFurthestX(image) + 1
+			y = findHighestY(image) - 2
+		elif "diacriticSpace" in character:
+			y += int(character["diacriticSpace"]) - 1
+		image = image | imageFromArray(arr, x, y)
+	return (image, kw)
+
+def findFurthestX(image):
+	for x in range(image.x_end - 1, image.x, -1):
+		for y in range(image.y, image.y_end):
+			if image[x, y]:
+				return x
+	return image.x
+
+def findHighestY(image):
+	for y in range(image.y_end - 1, image.y, -1):
+		for x in range(image.x, image.x_end):
+			if image[x, y]:
+				return y
+	return image.y
+
+def imageFromArray(arr, x=0, y=0):
+	return PixelImage(
+		x=x,
+		y=y,
+		width=len(arr[0]),
+		height=len(arr),
+		data=bytes(x for a in reversed(arr) for x in a),
+	)
+
+def drawImage(image, pen, *, dx=0, dy=0):
+	for polygon in generatePolygons(image):
+		start = True
+		for x, y in polygon:
+			x = (x + dx) * PIXEL_SIZE
+			y = (y + dy) * PIXEL_SIZE
+			if start:
+				pen.moveTo(x, y)
+				start = False
+			else:
+				pen.lineTo(x, y)
+		pen.closePath()
+
+generateFont()
+# generateExamples(characters, ligatures, charactersByCodepoint)
